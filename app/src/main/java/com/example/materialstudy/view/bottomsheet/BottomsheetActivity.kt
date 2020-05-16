0b73d68a52bbdf2ded5e3d30b2839a05e755e91c
victory316 <csh153@gmail.com>
2020-05-16 17:54:38 +0900

    Adding bottomsheet activity, testing

--- app/src/main/java/com/example/materialstudy/view/bottomsheet/BottomsheetActivity.kt
+++ app/src/main/java/com/example/materialstudy/view/bottomsheet/BottomsheetActivity.kt
@@ -2,13 +2,16 @@ package com.example.materialstudy.view.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.materialstudy.R
 import com.example.materialstudy.databinding.ActivityBottomsheetBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class BottomsheetActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityBottomsheetBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,18 @@ class BottomsheetActivity : AppCompatActivity() {
         binding = ActivityBottomsheetBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        setupBottomsheet()
+    }
+
+    fun setupBottomsheet() {
+        bottomSheetBehavior = (BottomSheetBehavior.from(binding.contentLayout)
+                as BottomSheetBehavior<View>)
+            .apply {
+
+                isHideable = false
+                state = BottomSheetBehavior.STATE_COLLAPSED
+                isFitToContents = true
+            }
     }
 }
