899bc583890f5ddf5337034acc1c12eb298a7a70
victory316 <csh153@gmail.com>
2020-05-16 15:25:29 +0900

    Testing chips

--- app/src/main/java/com/example/materialstudy/view/chips/ChipsActivity.kt
+++ app/src/main/java/com/example/materialstudy/view/chips/ChipsActivity.kt
@@ -3,10 +3,15 @@ package com.example.materialstudy.view.chips
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.materialstudy.databinding.ActivityChipsBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import timber.log.Timber
 
 class ChipsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityChipsBinding
+
+    private var chipIndex = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +19,17 @@ class ChipsActivity : AppCompatActivity() {
         binding = ActivityChipsBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.addChipButton.setOnClickListener {
+
+            Timber.d("adding chip! : $chipIndex")
+            val chip = Chip(this)
+            chip.text = "Chip $chipIndex"
+
+            chipIndex++
+
+            binding.chipGroup.addView(chip)
+            binding.chipGroup.invalidate()
+        }
     }
 }
