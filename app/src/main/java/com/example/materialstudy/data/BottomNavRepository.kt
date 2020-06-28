4e567e5c357032631935454da1b2263b22df8b93
victory316 <csh153@gmail.com>
2020-06-28 16:06:09 +0900

    Before add count buttons

---
+++ app/src/main/java/com/example/materialstudy/data/BottomNavRepository.kt
@@ -0,0 +1,9 @@
+package com.example.materialstudy.data
+
+// TODO 추후 업데이트
+class BottomNavRepository private constructor() {
+
+    companion object {
+        fun getInstance() = BottomNavRepository()
+    }
+}
--- app/src/main/java/com/example/materialstudy/viewmodel/BottomNavViewModel.kt
+++ app/src/main/java/com/example/materialstudy/viewmodel/BottomNavViewModel.kt
@@ -4,11 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.materialstudy.data.BottomNavRepository
 import com.example.materialstudy.data.MaterialRepository
 import timber.log.Timber
 
 class BottomNavViewModel internal constructor(
-    private val repository: MaterialRepository,
+    private val repository: BottomNavRepository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
